66a15690ea18973a446751fd6a958d5f3432c33e
Carrington <carryoby@gmail.com>
2024-03-13 02:00:48 +0000

    Update Partners.jsx
    Removed the vanilla javascript and made the images statics

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,21 +1,20 @@
+import React from 'react';
 import { partners } from "../assets/data";
 
 const Partners = () => {
   return (
     <section className="partner-logos mb-5 pb-5" id="partners">
-      <div className="container pl-5 pr-5">
+      <div className="container">
         <div className="mb-5 mt-5">
           <h2 className="text-center section-title">Partners</h2>
         </div>
 
-        <div className="swiper-container">
-          <div className="swiper-wrapper">
-            {partners.map((partner, index) => (
-              <div className="swiper-slide" key={index}>
-                <img src={partner.logo} alt={partner.alt} />
-              </div>
-            ))}
-          </div>
+        <div className="partners-wrapper d-flex flex-wrap justify-content-center align-items-center">
+          {partners.map((partner, index) => (
+            <div className="partner-item m-2" key={index}>
+              <img src={partner.logo} alt={partner.alt} style={{ maxWidth: "150px", height: "auto" }} />
+            </div>
+          ))}
         </div>
       </div>
     </section>
